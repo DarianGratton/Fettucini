75d404bb6684422960d06c8dc4d63b6de04439ef
Darian Gratton <Skullninja565@hotmail.com>
2017-04-13 11:03:49 -0700

    fixed

--- add_topic_form.php
+++ add_topic_form.php
@@ -1,24 +1,47 @@
 <?php
-	require_once('auth.php');
-?>
+	require_once('config.php');
 
+	// Connect to server and select database.
+	($GLOBALS["___mysqli_ston"] = mysqli_connect(DB_HOST,  DB_USER,  DB_PASSWORD))or die("cannot connect");
+	((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . constant('DB_DATABASE')))or die("cannot select DB");
+	$tbl_name="topic"; // Table name
+	
+	$sql="SELECT * FROM $tbl_name, members where members.member_id = topic.member_id ORDER BY id DESC";
+	// ORDER BY id DESC is order result by descending
+	$result=mysqli_query($GLOBALS["___mysqli_ston"], $sql);
+?>
+<?php
+	session_start();
+?>
+<?php
+	include 'functions.php';
+	require_once('config.php');
+	session_start();
+
+	// Connect to server and select database.
+	($GLOBALS["___mysqli_ston"] = mysqli_connect(DB_HOST,  DB_USER,  DB_PASSWORD))or die("cannot connect, error: ".((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
+	((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . constant('DB_DATABASE')))or die("cannot select DB, error: ".((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
+	$tbl_name="topic"; // Table name
+?>
 <html lang="en">
 <head>
   <title>Community</title>
   <meta charset="utf-8">
 </head>
-<link rel="stylesheet" href = "../style/base.css">
+<link rel="stylesheet" href = "style/base.css">
 <body>
 <div id="mainhome">
 
 <div id="header">
-<a href="index.html"><img src="../images/logo.png" width="254" height="100" alt="TechDirect"/></a>
-<div id="navsign">
-	<a href="login.html"><p>Log In</p></a>
-	</div>
-	<div id="navsign">
-	<a href="signup.html"><p>Sign Up</p></a>
-	</div>
+<a href="index.php"><img src="images/logo.png" width="254" height="100" alt="TechDirect"/></a>
+<?php
+		if (isLoggedIn()){
+			echo '<a href="logout.php"><button type="button" class="pointer">Logout</button></a>';
+			echo '<div id="welcome">',"welcome",'<br>',$_SESSION['SESS_FIRST_NAME'],'</div>';	
+		} else {
+			echo '<a href="signup.php"><button type="button" class="pointer">Sign Up/Login</button></a>';
+		}
+?>
 </div>
 
 <div id="nav">
@@ -27,16 +50,16 @@
 	<div class="dropdown">
 		<button class="dropbtn">Consoles</button>
 		<div class="dropdown-content">
-			<a href="pc.html" id="pcdropdown" style="a:hover {background-color: red;">PC</a>
-			<a href="xbox.html" id="xboxdropdown">Xbox One</a>
-			<a href="ps4.html" id="psdropdown">PS4</a>
-			<a href="switch.html" id="switchdropdown">Switch</a>
-			<a href="mobile.html" id="mobiledropdown">Mobile</a>
+			<a href="pc.php" id="pcdropdown" style="a:hover {background-color: red;">PC</a>
+			<a href="xbox.php" id="xboxdropdown">Xbox One</a>
+			<a href="ps4.php" id="psdropdown">PS4</a>
+			<a href="switch.php" id="switchdropdown">Switch</a>
+			<a href="mobile.php" id="mobiledropdown">Mobile</a>
 		</div>
 	</div>
 	</li>
-	<li><a href="directory.html">Directory</a></li>
-	<li><a href="./forum/forum.php">Community</a></li>
+	<li><a href="directory.php">Directory</a></li>
+	<li><a href="forum.php">Community</a></li>
 	</ul>
 </div>
 
@@ -75,5 +98,3 @@
 	</div>
 </body>
 </html>
-
-
